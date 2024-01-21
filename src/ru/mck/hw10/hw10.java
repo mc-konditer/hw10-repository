8dc75266c64077e84a83e0f05df692ac2c6e1571
ziryan <ziryan@bk.ru>
2024-01-21 17:20:01 +0800

    Complete

--- src/ru/mck/hw10/hw10.java
+++ src/ru/mck/hw10/hw10.java
@@ -3,6 +3,40 @@ package ru.mck.hw10;
 public class hw10 {
 
     public static void main(String[] args) {
+        task3();
+    }
+
+    public static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println(fullName);
+
+
+    }
+
+    public static void task2() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println(fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        fullName =fullName.replace('ё', 'е');
+
+        System.out.println(fullName);
 
     }
 }
